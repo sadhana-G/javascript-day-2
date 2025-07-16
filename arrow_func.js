f37cee1d6a139f17020e6e5f745e137c6739ecba
sadhana0728 <126068949+sadhana-G@users.noreply.github.com>
2025-07-16 12:43:35 +0530

    Add files via upload

---
+++ arrow_func.js
@@ -0,0 +1,62 @@
+/*console.log(a);
+   var a=10;
+console.log(a);
+
+//////////////// function ///////////////////
+    
+   function baby(color)
+   {
+    return "My Babies favorite color is :" + color;
+
+   }
+   let fav=baby("violet");
+   console.log(fav);
+*/
+
+/////////////// task /////////////////
+   const man = (a) =>
+   {
+    return "Our Company Name is :" + a;
+
+   }
+   let res=man("Hsanainfotech");
+   console.log(res);
+
+////////////////// add //////////////////
+
+   const add = (a,b) =>
+    {
+     return a + b ;
+ 
+    }
+    console.log(add(3,5));
+
+ //////////////////// sub /////////////////////
+
+    const sub = (a,b) =>
+        {
+         return a - b ;
+     
+        }
+        console.log(sub(3,5));
+
+///////////////////// square ////////////////
+        const squ = (a) =>
+            {
+             return a*a ;
+         
+            }
+            console.log(squ(5));
+
+/////////////////// cube ////////////////////////
+const cube= (a) =>
+    {
+     return a*a*a;
+ 
+    }
+    console.log(cube(3));
+
+///////////////// simple index /////////////////
+const add2 = (a,b) => a + b ;
+console.log(add2(3,5));
+
