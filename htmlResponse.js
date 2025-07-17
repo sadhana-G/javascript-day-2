ee5c32c20125a05cbcc76534906a974b3822211f
sadhana0728 <harinisana2003@gmail.com>
2025-07-17 13:38:25 +0530

    #first commit

--- htmlResponse.js
+++ htmlResponse.js
@@ -2,6 +2,6 @@ const http = require('http');
 const server = http.createServer((req,res)=>
 {
     res.setHeader('Content-Type', 'text/html');
-    res,ebd('<h1>Welcome to Node.js with HTML!</h1>');
+    res.end('<h1>Welcome to Node.js with HTML!</h1>');
 });
 server.listen(3000);
